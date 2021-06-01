6cc9769f82934587c549bef06d79051533c59c84
vinicius-lima-melo <viniciuslimamelo0@gmail.com>
2021-05-31 21:56:16 -0300

    first commit

---
+++ src/app/restaurant-detail/restaurant-detail.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router'
+import { Restaurant } from '../restaurants/restaurant/restaurant.model';
+import { RestaurantsService } from '../restaurants/restaurants.service';
+
+@Component({
+  selector: 'mt-restaurant-detail',
+  templateUrl: './restaurant-detail.component.html'
+})
+export class RestaurantDetailComponent implements OnInit {
+
+restaurant: Restaurant
+
+  constructor(private restaurantsService: RestaurantsService, private route: ActivatedRoute) { }
+
+  ngOnInit() {
+
+
+    //snapshot tira "foto" dos dados
+    //['id'] parametro definido nas rotas
+    this.restaurantsService.restaurantById(this.route.snapshot.params['id'])
+    .subscribe(restaurant => this.restaurant = restaurant)
+  }
+
+}
